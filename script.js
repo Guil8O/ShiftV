1178ae0da6616fadf0f3c56f2c75a6b32e218fa6
Guil8O <hynk88o@gmail.com>
2025-07-21 21:47:55 +0900

    Update script.js

--- script.js
+++ script.js
@@ -2396,7 +2396,7 @@ document.addEventListener('DOMContentLoaded', () => {
 }); // DOMContentLoaded End
 if ('serviceWorker' in navigator) {
   window.addEventListener('load', () => {
-    navigator.serviceWorker.register('/ShiftV/service-worker.js')
+    navigator.serviceWorker.register('service-worker.js')
       .then(registration => {
         console.log('Service Worker registered with scope:', registration.scope);
       })

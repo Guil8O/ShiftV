7d8383d977aa1b5205b70307188bb7f03214daf2
Guil8O <hynk88o@gmail.com>
2026-01-18 08:47:46 +0900

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'shiftv-v1.1.0';
+const CACHE_NAME = 'shiftv-v1.1.1';
 const urlsToCache = [
   '/ShiftV/',
   '/ShiftV/index.html',
@@ -39,4 +39,5 @@ self.addEventListener('fetch', function (event) {
     })
   );
 });
+
 
